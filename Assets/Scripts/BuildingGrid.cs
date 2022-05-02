78725f86ef1caa4cf63bb8f675596f7368a8e52f
Traksed <traksed@gmail.com>
2022-05-02 19:35:12 +0300

    Test Branch

---
+++ Assets/Scripts/BuildingGrid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BuildingGrid: MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello");
+        }
+
+        private void Update()
+        {
+            
+        }
+
+    }
+}
